a29224b7fe9ebc29b5e86c8134f72002d3b59e1f
Sergey Shtukin <sergey.shtukin.work@gmail.com>
2025-02-18 14:09:10 +0100

    Project Init

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ClickioSDK_Integration_Example_Android"
+include(":app")
+ 
