a86be1c4638b56fc50e3c45cae266536068133d1
Sergey Shtukin <sergey.shtukin.work@gmail.com>
2025-02-21 15:54:10 +0100

    update draft architecture

--- ClickioConsentSDK/src/main/java/com/clickio/clickioconsentsdk/ClickioConsentSDK.kt
+++ ClickioConsentSDK/src/main/java/com/clickio/clickioconsentsdk/ClickioConsentSDK.kt
@@ -9,6 +9,10 @@ class ClickioConsentSDK private constructor() {
 
         private const val CLICKIO_SDK_TAG = "CLICKIO_SDK"
 
+        private const val SCOPE_GDPR = "gdpr"
+        private const val SCOPE_US = "us"
+        private const val SCOPE_OUT_OF_SCOPE = "out_of_scope"
+
         private var instance: ClickioConsentSDK? = null
 
         /**
@@ -28,34 +32,26 @@ class ClickioConsentSDK private constructor() {
     private var onReadyListener: (() -> Unit)? = null
     private var consentStatus: ConsentStatus? = null
 
+    class ClickioInitError(reason: String) :
+        Throwable("Something went wrong during the initialization of ClickioConsentSDK: {$reason}")
 
-    enum class Scope {
-        GDPR,
-        US,
-        OUT_OF_SCOPE
-    }
+    data class Config(
+        val siteId: String,
+        val appLanguage: String? = null,
+    )
 
     enum class ConsentState {
-        CONSENT_NOT_APPLICABLE,
-        EU_USER_NOT_DECIDED_YET,
-        ER_USER_CONSENTED
-    }
-
-    enum class ConsentChoiceMade {
-        AGREE_ALL,
-        REJECT_ALL,
-        PARTIAL_AGREE
+        NOT_APPLICABLE,
+        GDPR_NO_DECISION,
+        GDPR_DECISION_OBTAINED,
+        US,
+        NOT_DEFINED
     }
 
     enum class DialogMode {
         DEFAULT,
         RESURFACE
     }
-
-    data class Config(
-        val clientId: String,
-        val appLanguage: String? = null,
-    )
 
     /**
      *  Logging class
@@ -190,8 +186,11 @@ class ClickioConsentSDK private constructor() {
         }
     }
 
-    private data class ConsentStatus(
-        val scope: Scope,
+    /**
+     * Data class for sdk/consent-status response
+     */
+    data class ConsentStatus(
+        val scope: String,
         val force: Boolean,
         val error: String,
     )
@@ -221,43 +220,25 @@ class ClickioConsentSDK private constructor() {
 
     /**
      * Description from client's documentation:
-     * Return the scope that applies to the user (may return GDPR or US or False).
+     * Return the scope that applies to the user (return the sdk/consent-status scope output).
      */
-    fun checkConsentScopeApplies() =
-        consentStatus?.scope //(Unless, of course, it is not important that it returns False instead of OUT_OF_SCOPE))
+    fun checkConsentScope(): String = consentStatus.scope
 
     /**
      * Description from client's documentation:
      * Return:
-     * consent not applicable (non-eu or US user)
-     * eu user, user has not decided yet
-     * eu user, user consented (does not mean, that user consented to everything)
+     * not_applicable (if scope = ‘out of scope’)
+     * gdpr_no_decision - scope = gdpr and force = true and force state is not changed during app session
+     * gdpr_decision_obtained - scope = gdpr and force = false
+     * us - scope = us
      */
     fun checkConsentState(): ConsentState {
-        // TODO: Declare whether force = user has not decided yet
-        return when (consentStatus.scope) {
-            Scope.GDPR -> if (consentStatus.force) ConsentState.ER_USER_CONSENTED else ConsentState.EU_USER_NOT_DECIDED_YET
-            Scope.US -> return ConsentState.CONSENT_NOT_APPLICABLE
-            Scope.OUT_OF_SCOPE -> return ConsentState.CONSENT_NOT_APPLICABLE
-        }
-    }
-
-    /**
-     * Description from client's documentation:
-     * Checks if consent choice is required from the user
-     * Return true if the user in the GDPR consent scope and no previous decision
-     */
-    fun checkConsentRequired(): Boolean {
-        // TODO: Declare whether force = user has not decided yet
-        return consentStatus == Scope.GDPR && consentStatus.force
-    }
-
-    /**
-     * Description from client's documentation:
-     * Determines whether the user has made a consent choice (agree all, reject all or partial agree).
-     */
-    fun checkIfConsentChoiceMade(): ConsentChoiceMade {
-        TODO("Determine whether how declare Agree All from Partial Agree")
+        if (consentStatus?.scope == SCOPE_OUT_OF_SCOPE) return ConsentState.NOT_APPLICABLE
+        if (consentStatus?.scope == SCOPE_GDPR && consentStatus?.force == true) return ConsentState.GDPR_NO_DECISION
+        if (consentStatus?.scope == SCOPE_GDPR && consentStatus?.force == false) return ConsentState.GDPR_DECISION_OBTAINED
+        if (consentStatus?.scope == SCOPE_US) return ConsentState.US
+        // For cases when scope from sdk/consent-status not described
+        return ConsentState.NOT_DEFINED
     }
 
     /**
@@ -277,27 +258,25 @@ class ClickioConsentSDK private constructor() {
 
 
     // WebView Screen Manipulations
-
     /**
      * Description from client's documentation:
      * Argument “mode”:
-     * Resurface - check if user in consent scope (not “out of scope”) and open the dialog
-     * Default - Checks if user's consent (GDPR) is required (user is in scope, no previous decision or previous decision is out of date) and open the dialog
+     * Resurface - check if user in consent scope (scope != out of scope) and open the dialog
+     * Default - if scope = gdpr and force = true then open the dialog
      * Argument “language” (optional) - force UI language
      */
     fun openDialog(
-        context: Context,
+        context: Context, // Android specific
         mode: DialogMode = DialogMode.DEFAULT,
         language: String? = null
     ) {
         when (mode) {
             DialogMode.DEFAULT -> {
-                // TODO: Declare whether force = no previous decision and how to check out of date
-                if (consentStatus?.scope == Scope.GDPR && consentStatus?.force == true) openWebViewActivity()
+                if (consentStatus?.scope == SCOPE_GDPR && consentStatus?.force == true) openWebViewActivity()
             }
 
             DialogMode.RESURFACE -> {
-                if (consentStatus?.scope == Scope.GDPR) openWebViewActivity()
+                if (consentStatus?.scope != SCOPE_OUT_OF_SCOPE) openWebViewActivity()
             }
         }
     }
@@ -306,8 +285,9 @@ class ClickioConsentSDK private constructor() {
      * Description from client's documentation:
      * Clears all consent data, effectively simulating the complete removal of the app from the device.
      */
-    fun clearData() {
+    private fun clearData() {
         // Wiping of SharedPreferences
+        // Available only for SDK developers for debugging
     }
 
     /**
@@ -316,7 +296,7 @@ class ClickioConsentSDK private constructor() {
     private fun fetchConsentStatus(): ConsentStatus {
         if (config == null) {
             logger.log("Missed configuration", EventLogger.EventLevel.ERROR)
-            return
+            throw ClickioInitError("Timeout exception") // Just example
         }
         // Calling GET https://clickiocdn.com/sdk/consent-status?s={config.clientId}&v={sharedPreferences.CLICKIO_CONSENT_server_request}
         // Getting Scope(GDPR, US, OUT_OF_SCOPE), force (True/False), error - parsing to ConsentStatus model
